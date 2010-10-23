d461d729c5a4af333edf5903a64dc6228cb7e4c9
buddhamagnet <buddhamagnet@gmail.com>
2010-10-23 14:13:28 +0100

    syntax error

--- bsbot.rb
+++ bsbot.rb
@@ -2,7 +2,7 @@ require 'cinch'
 
 messages = [
 'the nebulants report unusual activity on the feral register',
-'he blamed the static on interference at the komodo dragon level'
+'he blamed the static on interference at the komodo dragon level',
 'first world created',
 'analysing code for sklee residue'
 ''
